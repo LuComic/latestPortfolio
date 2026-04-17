64e49b063e683446d0a3e8de310270887b9b3192
Lukas Jääger <81866926+LuComic@users.noreply.github.com>
2026-04-17 17:12:33 +0300

    mobile changes for the portfolio template

--- src/routes/(projects)/one-portfolio-page/+page.svelte
+++ src/routes/(projects)/one-portfolio-page/+page.svelte
@@ -39,11 +39,11 @@
 	</div>
 {:else}
 	<div class="flex h-dvh w-full flex-col gap-4 overflow-y-auto px-4 py-2 text-white">
-		<div class="h-1/3 shrink-0">
-			<JohnBanner cols={5} rows={5} johnName={false} />
+		<div class="h-1/4 shrink-0">
+			<JohnBanner cols={5} rows={4} johnName={false} />
 		</div>
-		<h1 class="john-text shrink-0 text-2xl">About</h1>
-		<p class="text-lg text-gray-500">
+		<h1 class="john-text shrink-0 text-6xl">About</h1>
+		<p class="text-2xl text-gray-500">
 			Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci, cum! Veritatis debitis
 			voluptatibus eaque eligendi. Excepturi quod quibusdam deserunt. Iusto doloremque, ad obcaecati
 			pariatur aliquam impedit sapiente consequatur delectus natus consectetur, rem in cupiditate
